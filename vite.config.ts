c27c97dc64c59afc05b8eb312ef2bc74c9bb046a
braydanla <57197726+braydanla@users.noreply.github.com>
2026-01-03 23:53:03 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  
   server: {
     host: "::",
     port: 8080,
@@ -20,5 +18,10 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
 }));
